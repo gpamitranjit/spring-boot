1b964676b2194d3a41f32c67f2efa4f8e55be105
Amit Patil <gpamitranjit@gmail.com>
2020-02-23 15:32:11 +0530

    register the custom view controllers and protect the resources with spring security

---
+++ spring-boot-security/spring-security-register-view-controllers/src/main/java/com/amit/SpringSecurityRegisterViewControllersApplication.java
@@ -0,0 +1,13 @@
+package com.amit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRegisterViewControllersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRegisterViewControllersApplication.class, args);
+	}
+
+}
---
+++ spring-boot-security/spring-security-register-view-controllers/src/main/java/com/amit/web/security/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.amit.web.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+*
+* @author Amit Patil
+*
+**/
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+//		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		httpSecurity
+			.authorizeRequests().antMatchers("/", "/home").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+			.and()
+				.logout()
+				.permitAll();
+	}
+
+
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("amit").passwordEncoder((pass) -> {
+			return PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(pass);
+		})
+			.password("123")
+			.roles("ADMIN")
+			.build();
+		
+		return new InMemoryUserDetailsManager(user);
+	}
+}
