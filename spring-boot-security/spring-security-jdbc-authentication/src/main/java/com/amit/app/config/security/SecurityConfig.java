82765a90a7f72ec0468954a31cfee05bd6447619
Amit Patil <gpamitranjit@gmail.com>
2020-03-03 08:40:38 +0530

    spring security with jdbc authentication

---
+++ spring-boot-security/spring-security-jdbc-authentication/src/main/java/com/amit/app/config/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.amit.app.config.security;
+
+import java.util.ArrayList;
+import java.util.function.Function;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+
+/**
+*
+* @author Amit Patil
+*
+**/
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder authBuilder) throws Exception {
+		authBuilder
+			.jdbcAuthentication()
+			.dataSource(dataSource)
+			.withDefaultSchema()
+			.withUser(userBuilder().username("amit").password("123").authorities("ROLE_USER"))
+			.and()
+			.eraseCredentials(true);
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+			.authorizeRequests()
+			.antMatchers("/**").permitAll();
+//			.antMatchers("/h2-console/**").permitAll()
+//			.and()
+//			.authorizeRequests()
+//				.anyRequest().authenticated();
+		
+	}
+	
+	private UserBuilder userBuilder() {
+
+		Function<String, String> encoder = (rawPassword) -> {
+			return PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(rawPassword);
+		};
+		
+		UserBuilder userBuilder = User
+				.builder()
+				.passwordEncoder(encoder);
+		
+		return userBuilder;
+	}
+}
