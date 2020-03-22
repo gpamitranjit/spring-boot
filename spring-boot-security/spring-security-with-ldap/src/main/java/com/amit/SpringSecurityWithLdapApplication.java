d2f5b5c73b2bb881fac02ec1b0eb8285385c7ce7
Amit Patil <gpamitranjit@gmail.com>
2020-03-22 23:31:42 +0530

    spring-security integrating with LDAP for authentication

---
+++ spring-boot-security/spring-security-with-ldap/src/main/java/com/amit/SpringSecurityWithLdapApplication.java
@@ -0,0 +1,18 @@
+package com.amit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+*
+* @author Amit Patil
+*
+**/
+@SpringBootApplication
+public class SpringSecurityWithLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithLdapApplication.class, args);
+	}
+
+}
