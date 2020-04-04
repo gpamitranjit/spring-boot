9338209cf9f1a6aee223c3c902491f79b9dc6205
Amit Patil <gpamitranjit@gmail.com>
2020-04-04 13:14:19 +0530

    greetingService is mocked & /welcome endpoint is called using mockMvc object

--- spring-boot-web-layer-testing/src/test/java/com/amit/app/controller/TestWelcomeControllerWithWebLayerOnlyApplicationContext.java
+++ spring-boot-web-layer-testing/src/test/java/com/amit/app/controller/TestWelcomeControllerWithWebLayerOnlyApplicationContext.java
@@ -1,6 +1,7 @@
 package com.amit.app.controller;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -8,7 +9,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.amit.app.service.GreetingService;
 
 /**
 *
@@ -24,10 +28,16 @@ public class TestWelcomeControllerWithWebLayerOnlyApplicationContext {
 	@Autowired
 	private MockMvc mockmvc;
 	
+	@MockBean
+	private GreetingService greetingService;
+	
 	@Test
 	public void test_welcomeMessage_handle() throws Exception {
+		
+		when(greetingService.retrieveGreeting()).thenReturn("Welcome");
+		
 		this.mockmvc
-			.perform(get("/"))
+			.perform(get("/welcome"))
 			.andDo(print())
 			.andExpect(content().string(is("Welcome")));
 	}
