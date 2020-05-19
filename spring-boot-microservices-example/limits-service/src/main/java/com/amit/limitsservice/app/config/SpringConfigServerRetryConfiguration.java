ab21133246fbea405a51b53f09946d164c41c30e
Amit Patil <gpamitranjit@gmail.com>
2020-05-19 21:31:55 +0530

    added spring-retry feature for connecting to the config-server in all microservices

---
+++ spring-boot-microservices-example/limits-service/src/main/java/com/amit/limitsservice/app/config/SpringConfigServerRetryConfiguration.java
@@ -0,0 +1,27 @@
+package com.amit.limitsservice.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.interceptor.RetryInterceptorBuilder;
+import org.springframework.retry.interceptor.RetryOperationsInterceptor;
+
+/**
+*
+* @author Amit Patil
+*
+**/
+//@Configuration
+public class SpringConfigServerRetryConfiguration {
+
+// Disabled the manual registration spring-retry configuration
+// Kept this configuration just for reference that one can do the programatic configuration
+// of spring-rety
+	
+//	@Bean
+	public RetryOperationsInterceptor configServerRetryInterceptor() {
+		return RetryInterceptorBuilder.stateless()
+					.backOffOptions(2000, 3, 20000)
+					.maxAttempts(10)
+					.build();
+	}
+}
