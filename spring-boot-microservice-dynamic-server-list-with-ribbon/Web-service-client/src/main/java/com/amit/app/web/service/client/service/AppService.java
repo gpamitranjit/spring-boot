115cdbc09e3fced52e31b2ef7ec2774b8cf8933a
Amit Patil <gpamitranjit@gmail.com>
2020-09-05 00:19:43 +0530

    Revert "used feign client to create declerative REST client and request through feign clients are load balanced with ribbon imperatively"
    This reverts commit 1eacc79c0a8fff8caf724222888189ea9c44b1ac.

--- spring-boot-microservice-dynamic-server-list-with-ribbon/Web-service-client/src/main/java/com/amit/app/web/service/client/service/AppService.java
+++ spring-boot-microservice-dynamic-server-list-with-ribbon/Web-service-client/src/main/java/com/amit/app/web/service/client/service/AppService.java
@@ -2,20 +2,21 @@ package com.amit.app.web.service.client.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.amit.app.web.service.client.feign.WebServiceFeignClient;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AppService {
 
 	@Autowired
-	private WebServiceFeignClient webServiceFeignClient;
+	private RestTemplate restTemplate;
+	
+	private final String EUREKA_ZUUL_API_GATEWAY_NAME = "zuul-api-gateway";
 	
 	public String retrieveGreetingMessage(String userName) {
 
 		System.out.println("[web-service-client] [AppService] userName: " + userName);
 		
-		String greetingMessage = webServiceFeignClient.greeting(userName);
+		String greetingMessage = restTemplate.getForObject("http://"+EUREKA_ZUUL_API_GATEWAY_NAME + "/web-service/v1.0/greeting/" + userName, String.class);
 		
 		return greetingMessage;
 	}
