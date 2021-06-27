8a66f6ceb627a82b4e0bef268ae9cf10340f5bbc
Amit T Patil <gpamitranjit@gmail.com>
2021-06-27 12:16:32 +0530

    basic sprig-boot websocket app

---
+++ spring-boot-websocket/spring-boot-websocket-basic/src/main/java/com/amit/app/websocket/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.amit.app.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.amit.app.websocket.handler.MyTextWebSocketHandler;
+ 
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+ 
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(new MyTextWebSocketHandler(), "/web-socket");
+    }
+}
